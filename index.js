c3d25cc3ee8ea98b25f4250b92a630fd26ad2093
Simon Ndege <simopetley@gmail.com>
2025-06-20 11:49:19 +0300

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 
 let countEl = document.getElementById("count-el");
-
+let saveEl = document.getElementById("save-el");
 let count = 0;
 console.log(countEl);
 
@@ -11,3 +11,12 @@ function increment() {
     count= count + 1;
     countEl.innerText = count;
 }
+
+function save() {
+    let countStr = count + " - " ;
+   saveEl.textContent += countStr;
+   countEl.textContent=0;
+    count = 0;  
+   
+}
+
